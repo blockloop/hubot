306bd9f2e021c796aa9420446689bd320b6bba48
Brett Jones <blockloop@users.noreply.github.com>
2018-03-04 10:43:27 -0600

    add chuck norris joke

--- scripts/dadjokes.js
+++ scripts/dadjokes.js
@@ -20,8 +20,31 @@ module.exports = function(robot) {
 			} else if (body) {
 				msg.send(body);
 			} else {
-				robot.emit("error", `bad status code from http: (${res.statusCode})`);
+				throw new Error(`HTTP ${res.statusCode}: ${body || err}`);
 			}
+			msg.finish();
+		});
+	});
+	robot.respond(/tell me a chuck norris joke$/i, (msg) => {
+		const run = robot.http("https://api.icndb.com/jokes/random")
+			.header("User-Agent", "curl/7.54.0")
+			.header("Accept", "application/json")
+			.get();
+
+		run((err, res, body) => {
+			if (err) {
+				throw new Error(`HTTP Error: ${err}`);
+			}
+			if (res.statusCode > 299) {
+				throw new Error(`HTTP ${res.statusCode}: ${body || err}`);
+			}
+			const resp = JSON.parse(body);
+			if (resp.type === "success" && resp.value.joke) {
+				msg.send(resp.value.joke);
+				msg.finish();
+				return;
+			}
+			msg.send(body);
 			msg.finish();
 		});
 	});
