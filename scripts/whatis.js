992cbb1c8c8094ba8bfa7c2d17597efc891bf4a4
Brett Jones <blockloop@users.noreply.github.com>
2018-03-03 23:37:13 -0600

    whatis -> .js

---
+++ scripts/whatis.js
@@ -0,0 +1,76 @@
+// Description:
+//   Get system information from Hubot's home
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot what is your [hostname,loadavg,uptime,freemem,totalmem,cpus,type,release,networkInterfaces,arch,platform,tmpDir]?
+//   hubot what is your [name,version,deal,problem]?
+//   hubot what is your [key]
+//
+// Author:
+//   blockloop
+//
+const os = require("os");
+const start = new Date();
+const version = process.env.HUBOT_VERSION || "unknown";
+
+module.exports = function(robot) {
+	return robot.respond(/what (is|are) your (\w+)\??/i, (msg) => {
+		const query = msg.match[2].trim();
+		if (checkCustom(query, robot, msg)) {
+			return;
+		}
+
+		let result = "";
+		if (query === "uptime") {
+			result = prettyUptime(new Date() - start);
+		} else if (Reflect.has(os, query)) {
+			const prop = os[query];
+			if (typeof prop === "function") {
+				result = prop();
+			}
+		} else {
+			result = robot.brain.get(query);
+		}
+
+		if (result) {
+			if (typeof result === "object") {
+				result = JSON.stringify(result, null, 4);
+			}
+			msg.send(`My ${query} ${msg.match[1]} ${result}`);
+			msg.finish();
+			return;
+		}
+		msg.send(`I don't know. You tell me. Just say ${robot.name} your [key] is [value]`);
+		msg.finish();
+	});
+};
+
+function checkCustom(q, robot, msg) {
+	const done = (resp) => {
+		msg.reply(resp);
+		msg.finish();
+		return true;
+	};
+	if (q === "name") {
+		return done(`My name is ${robot.name}`);
+	}
+	if (q === "deal" || q === "problem") {
+		return done("@justin abused me");
+	}
+	if (q === "version") {
+		return done(`My version is ${version}`);
+	}
+	return false;
+}
+
+function prettyUptime(seconds) {
+	const date = new Date(null);
+	date.setSeconds(seconds / 1000);
+	return date.toISOString().substr(11, 8);
+}
