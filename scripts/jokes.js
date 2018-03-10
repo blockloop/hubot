c5446f74ceb533a0b7e694ccb4aacb62af2499be
Brett Jones <blockloop@users.noreply.github.com>
2018-03-09 22:48:34 -0600

    jokes: use request promise

--- scripts/jokes.js
+++ scripts/jokes.js
@@ -4,51 +4,36 @@
 // Commands:
 //   hubot tell me a (dad )joke - Reply with a joke
 //
+const request = require("request-promise-native");
 
 module.exports = function(robot) {
 	robot.respond(/tell me a (dad )?joke$/i, (msg) => {
-		new Promise((resolve) => {
-			robot.http("https://icanhazdadjoke.com/").
-			header("User-Agent", "curl/7.54.0").
-			header("Accept", "*/*").
-			get()((err, res, body) => {
-				if (err) {
-					throw new Error(`HTTP Error: ${err}`);
-				}
-				if (res.statusCode === 404) {
-					return resolve("Comic not found.");
-				}
-				if (res.statusCode > 299) {
-					throw new Error(`HTTP ${res.statusCode}: ${body || err}`);
-				}
-				return resolve(body);
-			});
+		request({
+			uri: "https://icanhazdadjoke.com/",
+			json: true,
+			headers: {
+				"User-Agent": "curl/7.54.0",
+				Accept: "*/*",
+			},
 		}).
-		then((body) => msg.send(body)).
+		then((resp) => msg.send(resp)).
 		catch((err) => robot.emit("error", err));
 	});
 
 	robot.respond(/tell me a chuck norris joke$/i, (msg) => {
-		new Promise((resolve) => {
-			robot.http("https://api.icndb.com/jokes/random").
-			header("User-Agent", "curl/7.54.0").
-			header("Accept", "application/json").
-			get()((err, res, body) => {
-				if (err) {
-					throw new Error(`HTTP Error: ${err}`);
-				}
-				if (res.statusCode > 299) {
-					throw new Error(`HTTP ${res.statusCode}: ${body || err}`);
-				}
-				return resolve(body);
-			});
+		request({
+			uri: "https://api.icndb.com/jokes/random",
+			json: true,
+			headers: {
+				"User-Agent": "curl/7.54.0",
+				Accept: "application/json",
+			},
 		}).
-		then((body) => JSON.parse(body)).
 		then((resp) => {
 			if (resp.type === "success" && resp.value && resp.value.joke) {
 				return resp.value.joke;
 			}
-			throw new Error(`unknown response from api: ${JSON.stringify(resp)}`);
+			return Promise.reject(JSON.stringify(resp));
 		}).
 		then((resp) => msg.send(resp)).
 		catch((err) => robot.emit("error", err));
