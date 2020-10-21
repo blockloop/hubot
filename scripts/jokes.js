b5598ab4634a932a816d13959e0a166f42985b4f
Justin Williams <justinjohnwilliams@gmail.com>
2020-10-21 09:05:21 -0500

    git + ipynb = :(

--- scripts/jokes.js
+++ scripts/jokes.js
@@ -5,6 +5,7 @@
 //   hubot tell me a (dad )joke - Reply with a joke
 //
 const request = require("request-promise-native");
+const apikey = process.env.HUBOT_DONALD_API_KEY;
 
 module.exports = function(robot) {
 	robot.respond(/tell me a (dad )?joke$/i, (msg) => {
@@ -32,6 +33,25 @@ module.exports = function(robot) {
 			then((resp) => {
 				if (resp.type === "success" && resp.value && resp.value.joke) {
 					return resp.value.joke;
+				}
+				return Promise.reject(JSON.stringify(resp));
+			}).
+			then((resp) => msg.send(resp)).
+			catch((err) => robot.emit("error", err));
+	});
+
+	robot.respond(/what would donald say$/i, (msg) => {
+		request({
+			uri: "https://matchilling-tronald-dump-v1.p.rapidapi.com/random/quote",
+			json: true,
+			headers: {
+				"x-rapidapi-key": apikey,
+				Accept: "application/json",
+			},
+		}).
+			then((resp) => {
+				if (resp.type === "success" && resp.value && resp.value.value) {
+					return resp.value.value;
 				}
 				return Promise.reject(JSON.stringify(resp));
 			}).
